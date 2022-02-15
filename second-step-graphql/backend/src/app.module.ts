9c65900195695f3bd82853e9402737877f246fe3
mrmtsu <81202muramatsu@gmail.com>
2022-02-16 06:46:20 +0900

    ✨ add apollo server

--- second-step-graphql/backend/src/app.module.ts
+++ second-step-graphql/backend/src/app.module.ts
@@ -1,15 +1,22 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import path from 'path';
+import * as path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsModule } from './components/posts/posts.module';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
-      autoSchemaFile: path.join(process.cwd(), 'src/schema.gql'),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: path.join(
+        process.cwd(),
+        'src/generated/graphql/schema.gql',
+      ),
       sortSchema: true,
     }),
+    PostsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
